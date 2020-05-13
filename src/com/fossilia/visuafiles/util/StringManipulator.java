2dc0abbd7beb3791578ada0707d2d5f65b188655
Faisal Bagalagel <uultradude@gmail.com>
2020-05-13 16:14:52 -0400

    Completely commented out StringManipulator class.

--- src/com/fossilia/visuafiles/util/StringManipulator.java
+++ src/com/fossilia/visuafiles/util/StringManipulator.java
@@ -2,17 +2,31 @@ package com.fossilia.visuafiles.util;
 
 import java.io.File;
 
+/**
+ * Class used for useful string manipulations
+ */
 public final class StringManipulator {
 
+    /**
+     * returns a string progress bar witha certain percentage
+     * @param percentage how much the bar should be filled
+     * @param divider what the increment amount should be for the progress bar (also affects size)
+     * @return the string progress bar
+     */
     public static String getProgressBar(double percentage, int divider){
-        String pb = "[";
+        StringBuilder pb = new StringBuilder("[");
         for(int i=0; i<(int)percentage/divider; i++){
-            pb+="#";
+            pb.append("#");
         }
-        pb+=String.format("%"+((100/divider+2)-percentage/divider)+"s", "]");
-        return pb;
+        pb.append(String.format("%" + ((100 / divider + 2) - percentage / divider) + "s", "]"));
+        return pb.toString();
     }
 
+    /**
+     * Converts bytes to bytes, KB, MB, or GB depending on which would be best for reading
+     * @param s the bytes to convert
+     * @return formatted string of converted bytes
+     */
     public static String convertSize(long s){
         double size = (double)s;
         if(size<1024){ //Byte range
@@ -32,6 +46,11 @@ public final class StringManipulator {
         }
     }
 
+    /**
+     * Gets the extension of a file
+     * @param f the file to get extension from
+     * @return extension of file if it has one, otherwise null
+     */
     public static String getExtension(File f){
         String path = f.getName();
         if(path.lastIndexOf(".")!=-1){
@@ -42,6 +61,11 @@ public final class StringManipulator {
         }
     }
 
+    /**
+     * Converts milliseconds to milliseconds, seconds, or minutes depending on which which would be best for reading
+     * @param time the milliseconds to be converted
+     * @return string version of converted time
+     */
     public static String convertTime(long time){
         if(time<1000){ //ms
             return String.format("%d ms", time);
